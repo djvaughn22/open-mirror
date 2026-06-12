908603ba3ee13534747fe3c894c5b34c701edd82
DJ Vaughn <djvaughn22@gmail.com>
2026-06-11 21:56:35 -0500

    Redirect Open Mirror domain to CrossHeartPray

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "openmirrorllc.com" }],
+        destination: "https://crossheartpray.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.openmirrorllc.com" }],
+        destination: "https://crossheartpray.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
