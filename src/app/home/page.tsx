3d9e5de3d6c48586022a79cb99b4933d6eece2e3
DJ Vaughn <djvaughn22@gmail.com>
2026-06-13 22:17:44 -0500

    Update Cross Heart Pray hero subtitle

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -76,7 +76,10 @@ export default function HomePage() {
           </h1>
 
           <p className="mx-auto mt-8 max-w-3xl text-2xl font-semibold leading-snug text-zinc-300 md:text-4xl">
-            your way through the Holy Bible
+            your way through the day
+          </p>
+          <p className="mx-auto mt-8 max-w-3xl text-2xl font-semibold leading-snug text-zinc-300 md:text-4xl">
+            with the Holy Bible
           </p>
 
           <p className="mx-auto mt-5 max-w-3xl text-xl font-semibold leading-snug text-blue-400 md:text-2xl">
