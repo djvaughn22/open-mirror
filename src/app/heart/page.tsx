aa0c7561e0497e08b41fecc72b00319ddda3a366
DJ Vaughn <djvaughn22@gmail.com>
2026-06-16 17:41:10 -0500

    Update Heart page hero line

--- src/app/heart/page.tsx
+++ src/app/heart/page.tsx
@@ -43,7 +43,7 @@ export default function HeartPage() {
           </h1>
 
           <p className="mx-auto mt-8 max-w-2xl text-xl font-semibold leading-9 text-blue-300 sm:text-2xl sm:leading-10">
-            Open your heart and receive God’s everlasting, unconditional love
+            Open your heart and receive God&apos;s Love
           </p>
           <p className="mx-auto mt-8 max-w-2xl text-xl font-semibold leading-9 text-zinc-300 sm:text-2xl sm:leading-10">
             God loves you deeply
