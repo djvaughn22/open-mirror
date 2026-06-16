f92a652e23c89eefb76c05ec19703318b0164b74
DJ Vaughn <djvaughn22@gmail.com>
2026-06-16 16:11:31 -0500

    Update home hero Bible app message

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -87,15 +87,13 @@ export default function HomePage() {
           </h1>
 
           <p className="mx-auto mt-8 max-w-3xl text-2xl font-semibold leading-snug text-zinc-300 md:text-4xl">
-            your way
+            Your Way
             <br />
-            throughout the day
-            <br />
-            with the Holy Bible
-</p>
+            in the HOLY BIBLE APP.
+          </p>
 
           <p className="mx-auto mt-5 max-w-3xl text-xl font-semibold leading-snug text-blue-400 md:text-2xl">
-            for Truth, Joy and Peace.
+            you will find true peace and joy
           </p>
         </section>
 
