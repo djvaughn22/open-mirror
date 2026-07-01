438dcf8be49b8f1eb000132be3c38c0f75b43cae
DJ Vaughn <djvaughn22@gmail.com>
2026-06-30 21:25:03 -0500

    temp: add dummy redirect to force routing table update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {};
-
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/__force-routing-update",
+        destination: "https://example.com",
+        permanent: false,
+      },
+    ];
+  },
+};
 export default nextConfig;
